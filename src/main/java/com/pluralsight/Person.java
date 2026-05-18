66aeb76679236e00ed232eefe13b0db9e186e590
yuujn <pale.cup5223@fastmail.com>
2026-05-18 15:28:25 -0500

    begin Exercise 2

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,4 @@
-package com.pluralsight.loops;
+package com.pluralsight;
 
 public class Person {
     private String firstName;
--- src/main/java/com/pluralsight/loops/Program.java
+++ src/main/java/com/pluralsight/loops/Program.java
@@ -1,5 +1,7 @@
 package com.pluralsight.loops;
 
+import com.pluralsight.Person;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
