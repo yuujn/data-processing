a16d4bde00df6b2785a73065973f551301aad2a8
yuujn <pale.cup5223@fastmail.com>
2026-05-18 15:31:32 -0500

    do step 1

--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -10,33 +10,17 @@ public class Program {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        List<Person> demoList = List.of(new Person[]{
-                new Person("John", "N", 25),
-                new Person("Johnson", "John", 40),
-                new Person("Josephson", "Johnson", 36),
-                new Person("Abigail", "Johnson", 43),
-                new Person("Johnson", "Abigail", 34),
-                new Person("Joan", "Joanson", 44),
-                new Person("John", "Joanson", 22),
-                new Person("Joan", "Johnson", 56),
-                new Person("Harriet", "The Spy", 11),
-                new Person("King", "Julian", 1111),
-        });
+        List<Person> demoList = List.of(new Person[]{new Person("John", "N", 25), new Person("Johnson", "John", 40), new Person("Josephson", "Johnson", 36), new Person("Abigail", "Johnson", 43), new Person("Johnson", "Abigail", 34), new Person("Joan", "Joanson", 44), new Person("John", "Joanson", 22), new Person("Joan", "Johnson", 56), new Person("Harriet", "The Spy", 11), new Person("King", "Julian", 1111),});
 
         System.out.print("First Name: ");
         String searchFirst = scan.nextLine();
 
-        ArrayList<Person> found = new ArrayList<>();
-
-        for (Person person : demoList) {
-            if (person.getFirstName().toLowerCase().contains(searchFirst.toLowerCase())) {
-                found.add(person);
-            }
-        }
-
-        for (Person person : found) {
-            System.out.println("Similar: " + person.getFullName());
-        }
+        List<Person> found = demoList.stream().filter(x -> {
+            String lower = x.getFirstName().toLowerCase();
+            String search = searchFirst.toLowerCase();
+            return lower.contains(search);
+        }).toList();
+        found.forEach(x -> System.out.println("Similar: " + x.getFullName()));
 
         double avgAge = 0.0;
         int min = demoList.get(0).getAge();
